84457c1c35dfbf39e1c85358b62924bc158f1cc9
Prince <Prince.chowdhury.246@my.csun.edu>
2023-07-22 03:23:15 -0400

    Creating navbar and adding clerk creds

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,12 @@ import { api } from "~/utils/api";
 import { ClerkProvider } from '@clerk/nextjs'; 
 
 import "~/styles/globals.css";
+import { Header } from "~/components/Header";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider {...pageProps}>
+      <Header />
       <Component {...pageProps} />
     </ClerkProvider>
   )
